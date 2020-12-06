aa5b550fd1ff2c3aa40df9b7890be006bfcff6d8
Ankur Vadhwani <www.nankur113@gmail.com>
2020-12-06 11:53:43 +0530

    a new router react example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./styles.css";
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+    </Router>
   );
 }
